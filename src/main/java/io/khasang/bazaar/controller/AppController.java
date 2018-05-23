06542e75b1bf84832eca7acc967f54687f793abc
SRBekmambetov <serg_bekm@mail.ru>
2018-05-23 23:57:24 +0300

    add abra cadabra

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -1,4 +1,11 @@
 package io.khasang.bazaar.controller;
 
 public class AppController {
+
+    public static void main(String[] args) {
+        System.out.println("dsf453sdf");
+        System.out.println("dsfs543df");
+        System.out.println("dsfs453df");
+        System.out.println("dsfs48df");
+    }
 }
